8cf53a3ebfa597dc8fde5c7ee0d4b5ca842a7f4a
whddbsl <whddbs04140@naver.com>
2024-12-05 20:55:41 +0900

    firebase 수정

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -6,9 +6,9 @@ const apps = getApps();
 if (!apps.length) {
   initializeApp({
     credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
   });
 }
